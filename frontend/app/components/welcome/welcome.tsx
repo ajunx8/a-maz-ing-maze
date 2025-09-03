e33813d104dec7942c7f01bce3b34c0a499401ea
Adrian Greksa <adrian.greksa@cresconet.com>
2025-09-03 10:39:55 +0100

    added docker compose setup for dev

---
+++ frontend/app/components/welcome/welcome.tsx
@@ -0,0 +1,9 @@
+export function Welcome() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">A-MAZE-ING-MAZE</h1>
+      <p className="text-lg">Hiii there!! Welcome to the A-MAZE-ING-MAZE!!!</p>
+    </main>
+  );
+}
+
--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,10 +1,10 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from "../components/welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "A-MAZE-ING-MAZE" },
+    { name: "description", content: "Welcome to the A-MAZE-ING-MAZE!" },
   ];
 }
 
