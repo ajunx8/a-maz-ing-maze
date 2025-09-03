91e252ec22180f1907c6ebc2060a208cb96e51ea
Adrian Greksa <adrian.greksa@cresconet.com>
2025-09-03 23:24:40 +0100

    update add --reload to backend/dockerfile to enable auto-reloading code changes

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/api/hello")
 def read_root():
     return {"message": "Hello from FastAPI!"}
+
+
+@app.get("/api/puzzles")
+def get_puzzles():
+    return {"puzzles": ["puzzle1", "puzzle2", "puzzle3"]}
